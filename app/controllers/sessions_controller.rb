099afcb3662110260f050c34f21d68a5d75e38d3
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-25 21:38:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     foursquare = FoursquareService.new
-    session[:token] = foursquare.authenticate!('LVJEVJ4BCTANBM5CY1FYBULOYBLIQXYHM4NM22HOTLVJCKAS'), '1GSGX0OQCLKOMYKISRAMFTGYPUSC00NGK1FJTB2WKSPO2QAY', params[:code])
+    session[:token] = foursquare.authenticate!('LVJEVJ4BCTANBM5CY1FYBULOYBLIQXYHM4NM22HOTLVJCKAS', '1GSGX0OQCLKOMYKISRAMFTGYPUSC00NGK1FJTB2WKSPO2QAY', params[:code])
     redirect_to root_path
   end
 
